3c1b4e91f2f8f62412b54f8fd06b1607b8c20b48
Youngha Park <youngha.mikoto@gmail.com>
2026-05-29 11:29:52 +0900

    feat(loader): get memory map and save to file

--- MisakaLoaderPkg/Main.c
+++ MisakaLoaderPkg/Main.c
@@ -28,6 +28,22 @@ EFI_STATUS EFIAPI UefiMain(
     EFI_HANDLE image_handle,
     EFI_SYSTEM_TABLE *system_table) {
   Print(L"Hello, Misaka World!\n");
+
+  CHAR8 memmap_buf[4096 * 4];
+  struct MemoryMap memmap = {sizeof(memmap_buf), memmap_buf, 0, 0, 0, 0};
+  GetMemoryMap(&memmap);
+
+  EFI_FILE_PROTOCOL* root_dir;
+  OpenRootDir(image_handle, &root_dir);
+
+  EFI_FILE_PROTOCOL* memmap_file;
+  root_dir->Open(
+      root_dir, &memmap_file, L"\\memmap",
+      EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE, 0);
+
+  SaveMemoryMap(&memmap, memmap_file);
+  memmap_file->Close(memmap_file);
+
   while (1);
   return EFI_SUCCESS;
 }
