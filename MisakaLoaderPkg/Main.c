b294199177613f5d3efb6a41f07b140f907d8491
Youngha Park <youngha.mikoto@gmail.com>
2026-05-29 14:28:36 +0900

    feat(loader): implement GetMemoryTypeUnicode function and OpenRootDir function

--- MisakaLoaderPkg/Main.c
+++ MisakaLoaderPkg/Main.c
@@ -27,6 +27,45 @@ EFI_STATUS GetMemoryMap(struct MemoryMap *map) {
                            &map->descriptor_version);
 }
 
+const CHAR16 *GetMemoryTypeUnicode(EFI_MEMORY_TYPE type) {
+  switch (type) {
+  case EfiReservedMemoryType:
+    return L"EfiReservedMemoryType";
+  case EfiLoaderCode:
+    return L"EfiLoaderCode";
+  case EfiLoaderData:
+    return L"EfiLoaderData";
+  case EfiBootServicesCode:
+    return L"EfiBootServicesCode";
+  case EfiBootServicesData:
+    return L"EfiBootServicesData";
+  case EfiRuntimeServicesCode:
+    return L"EfiRuntimeServicesCode";
+  case EfiRuntimeServicesData:
+    return L"EfiRuntimeServicesData";
+  case EfiConventionalMemory:
+    return L"EfiConventionalMemory";
+  case EfiUnusableMemory:
+    return L"EfiUnusableMemory";
+  case EfiACPIReclaimMemory:
+    return L"EfiACPIReclaimMemory";
+  case EfiACPIMemoryNVS:
+    return L"EfiACPIMemoryNVS";
+  case EfiMemoryMappedIO:
+    return L"EfiMemoryMappedIO";
+  case EfiMemoryMappedIOPortSpace:
+    return L"EfiMemoryMappedIOPortSpace";
+  case EfiPalCode:
+    return L"EfiPalCode";
+  case EfiPersistentMemory:
+    return L"EfiPersistentMemory";
+  case EfiMaxMemoryType:
+    return L"EfiMaxMemoryType";
+  default:
+    return L"InvalidMemoryType";
+  }
+}
+
 EFI_STATUS SaveMemoryMap(struct MemoryMap *map, EFI_FILE_PROTOCOL *file) {
   CHAR8 buf[256];
   UINTN len;
@@ -51,6 +90,23 @@ EFI_STATUS SaveMemoryMap(struct MemoryMap *map, EFI_FILE_PROTOCOL *file) {
                       desc->Attribute & 0xffffflu);
     file->Write(file, &len, buf);
   }
+
+  return EFI_SUCCESS;
+}
+
+EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL **root) {
+  EFI_LOADED_IMAGE_PROTOCOL *loaded_image;
+  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *fs;
+
+  gBS->OpenProtocol(image_handle, &gEfiLoadedImageProtocolGuid,
+                    (VOID **)&loaded_image, image_handle, NULL,
+                    EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+
+  gBS->OpenProtocol(loaded_image->DeviceHandle,
+                    &gEfiSimpleFileSystemProtocolGuid, (VOID **)&fs,
+                    image_handle, NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+
+  fs->OpenVolume(fs, root);
 
   return EFI_SUCCESS;
 }
