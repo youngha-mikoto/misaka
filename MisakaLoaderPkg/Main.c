81d1172f6ccfbe6fb8b29b7d58490b786a897064
Youngha Park <youngha.mikoto@gmail.com>
2026-05-29 09:28:09 +0900

    feat(loader): implement MemoryMap struct and GetMemoryMap function

--- MisakaLoaderPkg/Main.c
+++ MisakaLoaderPkg/Main.c
@@ -1,6 +1,29 @@
 #include <Uefi.h>
 #include <Library/UefiLib.h>
 
+struct MemoryMap {
+  UINTN buffer_size;
+  VOID* buffer;
+  UINTN map_size;
+  UINTN map_key;
+  UINTN descriptor_size;
+  UINT32 descriptor_version;
+};
+
+EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
+  if (map->buffer == NULL) {
+    return EFI_BUFFER_TOO_SMALL;
+  }
+
+  map->map_size = map->buffer_size;
+  return gBS->GetMemoryMap(
+    &map->map_size,
+    (EFI_MEMORY_DESCRIPTOR*)map->buffer,
+    &map->map_key,
+    &map->descriptor_size,
+    &map->descriptor_version);
+}
+
 EFI_STATUS EFIAPI UefiMain(
     EFI_HANDLE image_handle,
     EFI_SYSTEM_TABLE *system_table) {
