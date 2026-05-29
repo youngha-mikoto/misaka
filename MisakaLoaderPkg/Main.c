3c1590f599c0c4b809c4f245a32e1d7d94e8ae39
Youngha Park <youngha.mikoto@gmail.com>
2026-05-29 13:43:55 +0900

    style(loader): format code using clang-format (Google style)

--- MisakaLoaderPkg/Main.c
+++ MisakaLoaderPkg/Main.c
@@ -1,72 +1,65 @@
-#include <Uefi.h>
 #include <Library/UefiLib.h>
+#include <Uefi.h>
 
 struct MemoryMap {
   UINTN buffer_size;
-  VOID* buffer;
+  VOID *buffer;
   UINTN map_size;
   UINTN map_key;
   UINTN descriptor_size;
   UINT32 descriptor_version;
 };
 
-EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
+EFI_STATUS GetMemoryMap(struct MemoryMap *map) {
   if (map->buffer == NULL) {
     return EFI_BUFFER_TOO_SMALL;
   }
 
   map->map_size = map->buffer_size;
-  return gBS->GetMemoryMap(
-    &map->map_size,
-    (EFI_MEMORY_DESCRIPTOR*)map->buffer,
-    &map->map_key,
-    &map->descriptor_size,
-    &map->descriptor_version);
+  return gBS->GetMemoryMap(&map->map_size, (EFI_MEMORY_DESCRIPTOR *)map->buffer,
+                           &map->map_key, &map->descriptor_size,
+                           &map->descriptor_version);
 }
 
-EFI_STATUS SaveMemoryMap(struct MemoryMap* map, EFI_FILE_PROTOCOL* file) {
+EFI_STATUS SaveMemoryMap(struct MemoryMap *map, EFI_FILE_PROTOCOL *file) {
   CHAR8 buf[256];
   UINTN len;
 
-  CHAR8* header =
-    "Index, Type, Type(name), PhysicalStart, NumberOfPages, Attribute\n";
-  len = AsciiStrLen(header)'
-  file->Write(file, &len, header);
+  CHAR8 *header =
+      "Index, Type, Type(name), PhysicalStart, NumberOfPages, Attribute\n";
+  len = AsciiStrLen(header)' file->Write(file, &len, header);
 
-  Print(L"map->buffer = %08lx, map->map_size = %08lx\n",
-    map->buffer, map->map_size);
+      Print(L"map->buffer = %08lx, map->map_size = %08lx\n", map->buffer,
+            map->map_size);
 
   EFI_PHYSICAL_ADDRESS iter;
   int i;
   for (iter = (EFI_PHYSICAL_ADDRESS)map->buffer, i = 0;
-      iter < (EFI_PHYSICAL_ADDRESS)map->buffer + map->map_size;
-      iter += map->descriptor_size, i++) {
-    EFI_MEMORY_DESCRIPTOR* desc = (EFI_MEMORY_DESCRIPTOR*)iter;
-    len = AsciiSPrint(
-      buf, sizeof(buf),
-      "%u, %x, %-ls, %08lx, %lx, %lx\n",
-      i, desc->Type, GetMemoryTypeUnicode(desc->Type),
-      desc->PhysicalStart, desc->NumberOfPages,
-      desc->Attribute & 0xffffflu);
+       iter < (EFI_PHYSICAL_ADDRESS)map->buffer + map->map_size;
+       iter += map->descriptor_size, i++) {
+    EFI_MEMORY_DESCRIPTOR *desc = (EFI_MEMORY_DESCRIPTOR *)iter;
+    len = AsciiSPrint(buf, sizeof(buf), "%u, %x, %-ls, %08lx, %lx, %lx\n", i,
+                      desc->Type, GetMemoryTypeUnicode(desc->Type),
+                      desc->PhysicalStart, desc->NumberOfPages,
+                      desc->Attribute & 0xffffflu);
     file->Write(file, &len, buf);
   }
 
   return EFI_SUCCESS;
 }
 
-EFI_STATUS EFIAPI UefiMain(
-    EFI_HANDLE image_handle,
-    EFI_SYSTEM_TABLE *system_table) {
+EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
+                           EFI_SYSTEM_TABLE *system_table) {
   Print(L"Hello, Misaka World!\n");
 
   CHAR8 memmap_buf[4096 * 4];
   struct MemoryMap memmap = {sizeof(memmap_buf), memmap_buf, 0, 0, 0, 0};
   GetMemoryMap(&memmap);
 
-  EFI_FILE_PROTOCOL* root_dir;
+  EFI_FILE_PROTOCOL *root_dir;
   OpenRootDir(image_handle, &root_dir);
 
-  EFI_FILE_PROTOCOL* memmap_file;
+  EFI_FILE_PROTOCOL *memmap_file;
   root_dir->Open(
       root_dir, &memmap_file, L"\\memmap",
       EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE, 0);
@@ -74,7 +67,8 @@ EFI_STATUS EFIAPI UefiMain(
   SaveMemoryMap(&memmap, memmap_file);
   memmap_file->Close(memmap_file);
 
-  while (1);
+  while (1)
+    ;
   return EFI_SUCCESS;
 }
 
