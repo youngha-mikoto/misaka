864395bf1e9d98ffa4c464e09d1756a81b9078db
Youngha Park <youngha.mikoto@gmail.com>
2026-06-01 15:37:57 +0900

    kernel: Fix typo

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -2,7 +2,7 @@
 
 extern "C" void KernelMain(uint64_t frame_buffer_base,
                            uint64_t frame_buffer_size) {
-  uint8_t *frame_buffer = reinterpret_case<uint8_t *>(frame_buffer_base);
+  uint8_t *frame_buffer = reinterpret_cast<uint8_t *>(frame_buffer_base);
   for (uint64_t i = 0; i < frame_buffer_size; ++i) {
     frame_buffer[i] = i % 256;
   }
